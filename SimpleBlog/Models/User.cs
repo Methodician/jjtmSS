56acf535d2f54ba94e39e9f31aa06a0b153c8e82
Methodician <jacobgjohnston@gmail.com>
2015-11-12 17:47:08 -0800

    Wrapped up users admin and role editing features

--- SimpleBlog/Models/User.cs
+++ SimpleBlog/Models/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using NHibernate.Mapping.ByCode;
 using NHibernate.Mapping.ByCode.Conformist;
 
@@ -12,6 +12,13 @@ namespace SimpleBlog.Models
         public virtual string Username { get; set; }
         public virtual string Email { get; set; }
         public virtual string PasswordHash { get; set; }
+
+        public virtual IList<Role> Roles { get; set; }
+
+        public User()
+        {
+            Roles = new List<Role>();
+        }
 
         public static void FakeHash()
         {
@@ -45,6 +52,12 @@ namespace SimpleBlog.Models
                 x.Column("password_hash");
                 x.NotNullable(true);
             });
+
+            Bag(x => x.Roles, x =>
+            {
+                x.Table("role_users");
+                x.Key(k => k.Column("user_id"));
+            }, x => x.ManyToMany(k => k.Column("role_id"))); 
         }
     }
 }
